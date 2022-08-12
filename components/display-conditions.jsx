75b70a11312255b9723fb3cebc293042e0a393f2
ccrudkin <ccrudkin@gmail.com>
2022-08-12 14:25:33 -0600

    finish recent-common switch refactor

--- components/display-conditions.jsx
+++ components/display-conditions.jsx
@@ -4,15 +4,7 @@ import { processConditions } from './trail-conditions';
 import RecentCommonSwitch from './recent-common-switch';
 
 export default function DisplayConditions({ condSubmit, setCondSubmit, condition, localeId }) {
-  // use a toggle to choose 'most recent condition' OR 'most reported condition'
-  // JSX to display can be rendered based on the state of the toggle
-  // display will get data from displayReports state below
   const [toggle, setToggle] = useState('recent');
-
-  // process conditions and store ready-to-display versions in state
-  // i.e., 'most recent condition' + human readable date
-  // 'most reported condition' (in the last 30 days) + number of times reported
-  // setDisplayReports should run in an effect that monitors changes in 'condition' prop
   const [displayReports, setDisplayReports] = useState(null);
 
   useEffect(() => {
@@ -22,9 +14,7 @@ export default function DisplayConditions({ condSubmit, setCondSubmit, condition
   }, [condition]);
 
   const handleToggle = (e) => {
-    e.target.checked
-    ? setToggle('common')
-    : setToggle('recent')
+    e.target.checked ? setToggle('common') : setToggle('recent');
   };
 
   return (
@@ -32,26 +22,6 @@ export default function DisplayConditions({ condSubmit, setCondSubmit, condition
       <div className="col-md-12">
         <h3>Trail Conditions</h3>
         {
-          // this switch could probably become a component
-          // displayReports && displayReports[toggle].condition !== 'nodata'
-          // ? <div className="recentCommonContainer mt-2 mb-3 ms-2">
-          //     <div className="recentCommonLabel">Most Recent</div>
-          //     <label htmlFor="recentCommonSwitch" className="switch">
-          //       <input type="checkbox" id="recentCommonSwitch" onClick={handleToggle} role="switch" />
-          //       <span className="slider round"></span>
-          //     </label>
-          //     <div className="recentCommonLabel">Most Common</div>
-          //   </div>
-          // : displayReports && displayReports[toggle].condition === 'nodata'
-          // ? <div className="recentCommonContainer mt-2 mb-3 ms-2">
-          //     <div className="recentCommonLabel">Most Recent</div>
-          //     <label htmlFor="recentCommonSwitch" className="switch switch-disabled">
-          //       <input type="checkbox" id="recentCommonSwitch" role="switch" aria-disabled="true" />
-          //       <span className="slider round"></span>
-          //     </label>
-          //     <div className="recentCommonLabel">Most Common</div>
-          //   </div>
-          // : <p><span className="loading-placeholder loading-text"></span></p>
           displayReports
           ? <RecentCommonSwitch condition={displayReports[toggle.condition]} handleToggle={handleToggle} />
           : <p><span className="loading-placeholder loading-text"></span></p>
